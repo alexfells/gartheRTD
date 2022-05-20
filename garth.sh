08827837842bd62cb57bece86e583b4773f83ddb
alexfells <pm11af@leeds.ac.uk>
2022-05-20 22:43:33 +0100

    add run script

---
+++ garth.sh
@@ -0,0 +1,6 @@
+#$ -cwd -V					                # Run in current working directory
+#$ -l h_rt=48:00:00		    		        # Set a time limit hh:mm:ss. 48 hr max
+#$ -l h_vmem=8G
+#$ -m be					                # Email at start of job
+#$ -M pm11af@leeds.ac.uk		    	    # Specifies email
+python -u ~/CompArt/CompArt_AF/CompArt_AF/Simulate.py > run.log
