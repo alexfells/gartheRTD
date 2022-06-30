528f776e221984753247ca6a826ea782715b3361
alexfells <pm11af@leeds.ac.uk>
2022-06-30 10:47:13 +0100

    update runscript to ensure adaquate memory for large sims

--- garth.sh
+++ garth.sh
@@ -1,6 +1,7 @@
 #$ -cwd -V					                # Run in current working directory
 #$ -l h_rt=48:00:00		    		        # Set a time limit hh:mm:ss. 48 hr max
-#$ -l h_vmem=8G
+#$ -l h_vmem=64G
+#$ -l disk=32G
 #$ -m be					                # Email at start of job
 #$ -M pm11af@leeds.ac.uk		    	    # Specifies email
 python -u ~/CompArt/CompArt_AF/CompArt_AF/Simulate.py > run.log
